a7a8a99c71f93b73d6c7b73ad726d99c33180453
Ameya Ganore <ameyaganore@gmail.com>
2020-10-09 11:16:55 +0530

    Initial Commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ const Navbar = () => {
+    return (
+        <nav className='navbar navbar-dark bg-dark mb-5'>
+            <span className="navbar-brand mb-0h1 mx-auto">LyRicsFinder</span>
+
+        </nav>
+        
+    )
+}
+
+export default Navbar
