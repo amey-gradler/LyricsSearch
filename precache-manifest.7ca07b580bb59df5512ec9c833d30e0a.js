92f1b18b17a21d8dc65b2709e829204d22681dc2
Ameya Ganore <ameyaganore@gmail.com>
2021-10-04 20:03:24 +0530

    Updates

---
+++ precache-manifest.7ca07b580bb59df5512ec9c833d30e0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0366a99dd533aa9b185324cec89294a9",
+    "url": "/lyrics_app/index.html"
+  },
+  {
+    "revision": "9dcd36154fd9dec33909",
+    "url": "/lyrics_app/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c155bd59776711e6560c",
+    "url": "/lyrics_app/static/js/2.e15a324b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lyrics_app/static/js/2.e15a324b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dcd36154fd9dec33909",
+    "url": "/lyrics_app/static/js/main.197a8a39.chunk.js"
+  },
+  {
+    "revision": "edc34ecc4ed36fa59214",
+    "url": "/lyrics_app/static/js/runtime-main.f6d52347.js"
+  }
+]);
